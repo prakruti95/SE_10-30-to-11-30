247d2a6aff52c538eb4be94a98a336d89add71fe
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-05-30 17:00:31 +0530

    commit

---
+++ main (82).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,add;
+    
+    printf("Enter Number 1:");
+    scanf("%d",&a);
+    
+    printf("Enter Number 2:");
+    scanf("%d",&b);
+    
+    add = a+b;
+    printf("Answer is %d",add);
+    getch();
+}
