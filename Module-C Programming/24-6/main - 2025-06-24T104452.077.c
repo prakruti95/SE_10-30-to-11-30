1b17f4cf3fe3462f83a2f8185ac97f2e76fb0c60
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-06-24 11:25:34 +0530

    commit

---
+++ Module-C Programming/24-6/main - 2025-06-24T104452.077.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    const float PI = 3.14;
+    
+    PI= 6.28;
+    
+    printf("%f",PI);
+    
+    
+    
+    
+    getch();
+}
---
+++ Module-C Programming/24-6/main - 2025-06-24T111759.485.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   
+    int n, sum = 0, i = 0 ;
+   
+   printf("Enter Your Number");
+   scanf("%d",&n);//5
+   
+    tops:
+    i++ ;   //1//2//3//4//5//6                                  
+    sum += i ; //sum=sum+i//1
+    
+    
+    if (i < n) 
+	goto tops;
+	
+	printf ("\n sum of %d natural numbers = %d", n, sum) ;
+    
+    
+    
+    getch();
+}
