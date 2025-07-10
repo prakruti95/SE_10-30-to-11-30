8e21863794f1fe6381e029f4fdee0dd2e7f6d74c
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-07-10 11:58:17 +0530

    commit

---
+++ main - 2025-07-10T110502.126.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int  num = 13;
+    char c = 'k'; /* ASCII value is 107 */
+    float sum;
+    sum = num + c;
+    printf("sum = %f\n", sum );
+    
+    return 0;
+}
